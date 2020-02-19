24e9513cc8c1f32da29281a7d6544f02ff59cf2d
Cheik-Siramakan Keita <cheiksiramakankeita@gmail.com>
2020-02-19 21:27:02 +0100

    Test de connection de le base de données.

--- conf/connect.php
+++ conf/connect.php
@@ -1,15 +1,19 @@
 <?php
 class Connect{
 	public function connexion($type){
+		/*
 		$settings = parse_ini_file("settings.ini", true);
 		$host = $settings[$type]["host"];
 		$port = $settings[$type]["port"];
 		$db = $settings[$type]["db"];
 		$user = $settings[$type]["user"];
 		$password = $settings[$type]["password"];
-
+		*/
+		$user = "ckeita";
+		$password = "@Crownclown_91";
+		$db = "";
 		try{
-			$db = new PDO($type.":host=".$host.";port=".$port.";dbname=".$db.";", $user, $password);
+			$db = new PDO("mysql:host=192.168.1.10;port=;dbname=telemedecine;", $user, $password);
 		}catch(Exception $e){
 			echo "Échec lors de la connexion: ".$e->getMessage();
 		}
